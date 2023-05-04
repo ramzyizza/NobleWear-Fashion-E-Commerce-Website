ef5436f3ac87f500dfb5b07e246ce0bbe5303505
Irf Fikr <oryza4444@gmail.com>
2023-05-04 11:11:22 +0700

    Update server.js

--- utils/server.js
+++ utils/server.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://ory-shop.herokuapp.com/';
+export const server = dev ? 'http://localhost:3000' : 'https://ory-shop.vercel.app/';
