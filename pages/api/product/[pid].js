48d5ae58dd0ad2a687729f2f8d9a6504d2d7c6be
DSJet <teddyantonius7@gmail.com>
2023-06-04 12:27:30 +0700

    integrate midtrans for transaction

--- pages/api/product/[pid].js
+++ pages/api/product/[pid].js
@@ -1,11 +1,11 @@
 // fake data
-import products from '../../../utils/data/products';
+import products from "../../../utils/data/products";
 
 export default (req, res) => {
   const {
     query: { pid },
-  } = req
+  } = req;
 
-  const product = products.find(x => x.id === pid);
+  const product = products.find((x) => x.id === pid);
   res.status(200).json(product);
-}
+};
