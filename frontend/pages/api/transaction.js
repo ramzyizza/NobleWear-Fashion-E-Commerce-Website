7fe2b8e6a5ed2993deb424133fdb16ec9de11523
DSJet <teddyantonius7@gmail.com>
2023-06-04 21:52:35 +0700

    integrate midtrans transaction

---
+++ frontend/pages/api/transaction.js
@@ -0,0 +1,39 @@
+const midtransClient = require("midtrans-client");
+
+let snap = new midtransClient.Snap({
+  isProduction: false,
+  serverKey: "SB-Mid-server-IwTgG4fl_D77N4Y2KSLboaxJ",
+});
+
+let parameter = {
+  transaction_details: {
+    order_id: "YOUR-ORDERID-123456",
+    gross_amount: 10000,
+  },
+  credit_card: {
+    secure: true,
+  },
+  customer_details: {
+    first_name: "budi",
+    last_name: "pratama",
+    email: "budi.pra@example.com",
+    phone: "08111222333",
+  },
+};
+
+let transactionToken; // Declare the variable outside the `then` block
+
+export default (req, res) => {
+  const data = req.body;
+
+  // Use the transactionToken variable here
+  snap.createTransaction(parameter).then((transaction) => {
+    // Assign the value to the variable inside the `then` block
+    transactionToken = transaction.token;
+    res.status(200).json({
+      message: "success",
+      transactionToken: transactionToken,
+      redirect_url: transaction.redirect_url,
+    });
+  });
+}; 
--- frontend/pages/cart/checkout.jsx
+++ frontend/pages/cart/checkout.jsx
@@ -1,29 +1,130 @@
-import Layout from '../../layouts/Main';
-import { useSelector } from 'react-redux';
-import CheckoutStatus from '../../components/checkout-status';
-import CheckoutItems from '../../components/checkout/items';
-import { useRef } from 'react';
-
+import { useSelector } from "react-redux";
+import { useRef, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useFormik } from "formik";
+import Link from "next/link";
+import Layout from "../../layouts/Main";
+import CheckoutStatus from "../../components/checkout-status";
+import CheckoutItems from "../../components/checkout/items";
+import { selectTotalPrice } from "../../store/reducers/cart";
 
 const CheckoutPage = () => {
+  const router = useRouter();
+  const [data, setData] = useState({});
+  const priceTotal = useSelector((state) => state.cart.cartItems).reduce(
+    (acc, item) => acc + item.price * item.count,
+    0
+  );
 
-  const priceTotal = useSelector(state => {
-    const cartItems = state.cart.cartItems;
-    let totalPrice = 0;
-    if(cartItems.length > 0) {
-      cartItems.map(item => totalPrice += item.price * item.count);
-    }
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      address: "",
+      firstName: "",
+      lastName: "",
+      phone: "",
+      city: "",
+      postalCode: "",
+      country: "",
+      amount: priceTotal,
+    },
 
-    return totalPrice;
-  })
+    onSubmit: async (values) => {
+      console.log(values);
+      const response = await fetch("/api/transaction", {
+        method: "POST",
+        body: JSON.stringify(values),
+      });
+      const data = await response.json();
+      console.log(data);
+    },
+  });
 
-  const submitPayment = useRef(null); 
+  const submitPayment = useRef(null);
 
-  const handleClick = (e) => {
-    submitPayment.current?.click();
-    
-    console.log('triggerred')
-  }
+  const handleClick = () => {
+    const transactionToken = data.transactionToken;
+    const amount = data.amount;
+    const currency = data.currency;
+    const orderId = data.orderId;
+
+    window.snap.pay({
+      token: transactionToken,
+      amount: amount,
+      currency: currency,
+      orderId: orderId,
+      // Other configuration options for the payment gateway
+    });
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/transaction");
+        if (response.ok) {
+          const data = await response.json();
+          setData(data);
+        }
+      } catch (error) {
+        console.error("Failed to fetch data from API");
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // router.push("/transaction");
+    console.log(formik.values);
+    const response = await fetch("/api/transaction", {
+      method: "POST",
+    });
+    const data = await response.json();
+
+    const redirectUrl = data.redirect_url;
+
+    window.snap.pay(data.transactionToken, {
+      onSuccess: function (result) {
+        console.log("success");
+        console.log(result);
+        router.push("/transaction");
+      },
+      onPending: function (result) {
+        console.log("pending");
+        console.log(result);
+        router.push("/transaction");
+      },
+      onError: function (result) {
+        console.log("error");
+        console.log(result);
+        router.push("/transaction");
+      },
+    });
+  };
+
+  useEffect(() => {
+    // You can also change below url value to any script url you wish to load,
+    // for example this is snap.js for Sandbox Env (Note: remove `.sandbox` from url if you want to use production version)
+    const midtransScriptUrl = "https://app.sandbox.midtrans.com/snap/snap.js";
+
+    let scriptTag = document.createElement("script");
+    scriptTag.src = midtransScriptUrl;
+
+    // Optional: set script attribute, for example snap.js have data-client-key attribute
+    // (change the value according to your client-key)
+    const myMidtransClientKey = "your-client-key-goes-here";
+    scriptTag.setAttribute("data-client-key", myMidtransClientKey);
+
+    document.body.appendChild(scriptTag);
+
+    return () => {
+      document.body.removeChild(scriptTag);
+    };
+  }, []);
+
+  // Then somewhere else on your React component, `window.snap` global object will be available to use
+  // e.g. you can then call `window.snap.pay( ... )` function.
 
   return (
     <Layout>
@@ -37,121 +138,175 @@ const CheckoutPage = () => {
           <div className="checkout-content">
             <div className="checkout__col-6">
               <div className="checkout__btns">
-                <a href='/login'><button className="btn btn--rounded btn--yellow">Log in</button></a>
-                <a href='/register'><button className="btn btn--rounded btn--border">Sign up</button></a>
+                <Link href="/login" passHref>
+                  <span>
+                    <button className="btn btn--rounded btn--yellow">
+                      Log in
+                    </button>
+                  </span>
+                </Link>
+                <Link href="/register" passHref>
+                  <span>
+                    <button className="btn btn--rounded btn--border">
+                      Sign up
+                    </button>
+                  </span>
+                </Link>
               </div>
 
               <div className="block">
                 <h3 className="block__title">Shipping information</h3>
-                <form className="form" action="https://row-shop.herokuapp.com/get-token/" method="GET">
+                <form className="form" onSubmit={handleSubmit}>
                   <div className="form__input-row form__input-row--two">
                     <div className="form__col">
-                      <input className="form__input form__input--sm" name="email" refs="email" type="email" placeholder="Email" required/>
+                      <input
+                        className="form__input form__input--sm"
+                        name="email"
+                        refs="email"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        type="email"
+                        placeholder="Email"
+                        required
+                      />
                     </div>
 
                     <div className="form__col">
-                      <input className="form__input form__input--sm" name="address" type="text" placeholder="Address" />
-                    </div>
-                  </div>
-                  
-                  <div className="form__input-row form__input-row--two">
-                    <div className="form__col">
-                      <input className="form__input form__input--sm" name="name" type="text" placeholder="First name" />
-                    </div>
-
-                    <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="City" />
-                    </div>
-                  </div>
-                  
-                  <div className="form__input-row form__input-row--two">
-                    <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="Last name" />
-                    </div>
-
-                    <div className="form__col">
-                      <input className="form__input form__input--sm" type="text" placeholder="Postal code / ZIP" />
+                      <input
+                        className="form__input form__input--sm"
+                        name="address"
+                        type="text"
+                        value={formik.values.address}
+                        onChange={formik.handleChange}
+                        placeholder="Address"
+                      />
                     </div>
                   </div>
 
                   <div className="form__input-row form__input-row--two">
                     <div className="form__col">
-                      <input className="form__input form__input--sm" name="phone" type="text" placeholder="Phone number" />
-                      <input type="hidden" name="amount" value={priceTotal}/>
+                      <input
+                        className="form__input form__input--sm"
+                        name="firstName"
+                        id="firstName"
+                        type="text"
+                        placeholder="First name"
+                        value={formik.values.firstName}
+                        onChange={formik.handleChange}
+                      />
+                    </div>
 
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="City"
+                        name="city"
+                        id="city"
+                        value={formik.values.city}
+                        onChange={formik.handleChange}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="form__input-row form__input-row--two">
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        placeholder="Last name"
+                        id="lastName"
+                        name="lastName"
+                        value={formik.values.lastName}
+                        onChange={formik.handleChange}
+                      />
+                    </div>
+
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        type="text"
+                        id="postalCode"
+                        placeholder="Postal code / ZIP"
+                        value={formik.values.postalCode}
+                        onChange={formik.handleChange}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="form__input-row form__input-row--two">
+                    <div className="form__col">
+                      <input
+                        className="form__input form__input--sm"
+                        name="phone"
+                        type="text"
+                        placeholder="Phone number"
+                        value={formik.values.phone}
+                        onChange={formik.handleChange}
+                      />
+                      <input type="hidden" name="amount" value={priceTotal} />
                     </div>
 
                     <div className="form__col">
                       <div className="select-wrapper select-form">
-                        <select>
+                        <select
+                          value={formik.values.country}
+                          onChange={formik.handleChange}
+                          id="country"
+                        >
                           <option>Country</option>
                           <option value="Indonesia">Indonesia</option>
                         </select>
                       </div>
                     </div>
                   </div>
-                   <button ref={submitPayment} style={{display: "none"}} type="submit" className="btn pay btn--rounded btn--yellow">Proceed to payment 2</button>
+                  <button
+                    ref={submitPayment}
+                    type="submit"
+                    className="btn pay btn--rounded btn--yellow"
+                  >
+                    Proceed to payment
+                  </button>
                 </form>
               </div>
             </div>
-            
-            <div className="checkout__col-4">
-              <div className="block">
-                <h3 className="block__title">Payment method</h3>
-                <ul className="round-options round-options--three">
-                  <li className="round-item">
-                    <img src="/images/logos/new/credit_card.png" alt="cc" />
-                  </li>
-                  <li className="round-item">
-                    <img src="/images/logos/new/bank_transfer.png" alt="bank transfer" />
-                  </li>
-                  <li className="round-item">
-                    <img src="/images/logos/new/gopay.png" alt="gopay" />
-                  </li>
-                  <li className="round-item">
-                    <img src="/images/logos/new/shopee_pay.png" alt="shopee pay" />
-                  </li>
-               
-                </ul>
-              </div>
-              
-              <div className="block">
-                <h3 className="block__title">Delivery method</h3>
-                <ul className="round-options round-options--two">
-                  <li className="round-item round-item--bg">
-                    <img src="/images/logos/new/sicepat.png" alt="sicaepat" />
-                    {/* <p>$20.00</p> */}
-                  </li>
-              
-                </ul>
-              </div>
+
+            <div className="block">
+              <h3 className="block__title">Delivery method</h3>
+              <ul className="round-options round-options--two">
+                <li className="round-item round-item--bg">
+                  <img src="/images/logos/new/sicepat.png" alt="sicaepat" />
+                </li>
+              </ul>
             </div>
-            
-            <div className="checkout__col-2">
-              <div className="block">
-                <h3 className="block__title">Your cart</h3>
-                <CheckoutItems />
-                
-                <div className="checkout-total">
-                  <p>Total cost</p>
-                  <h3>${priceTotal}</h3>
-                </div>
+          </div>
+
+          <div className="checkout__col-2">
+            <div className="block">
+              <h3 className="block__title">Your cart</h3>
+              <CheckoutItems />
+
+              <div className="checkout-total">
+                <p>Total cost</p>
+                <h3>${priceTotal}</h3>
               </div>
             </div>
           </div>
-          
-          <div className="cart-actions cart-actions--checkout">
-            <a href="/cart" className="cart__btn-back"><i className="icon-left"></i> Back</a>
-            <div className="cart-actions__items-wrapper">
-              <button type="button" className="btn btn--rounded btn--border">Continue shopping</button>
-              <button onClick={handleClick} type="button" className="btn btn--rounded btn--yellow">Proceed to payment</button>
-            </div>
+        </div>
+
+        <div className="cart-actions cart-actions--checkout">
+          <Link href="/cart" passHref className="cart__btn-back">
+            <i className="icon-left"></i> Back
+          </Link>
+          <div className="cart-actions__items-wrapper">
+            <button type="button" className="btn btn--rounded btn--border">
+              Continue shopping
+            </button>
           </div>
         </div>
       </section>
     </Layout>
-  )
+  );
 };
 
-  
-export default CheckoutPage
+export default CheckoutPage;
